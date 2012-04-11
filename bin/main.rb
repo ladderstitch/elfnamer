47260fc17a47e1c9781bb627fcdd039292c66253
Jennifer Nordquist <nordquist.jennifer@gmail.com>
2012-04-10 20:46:06 -0400

    name patterns added

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,12 @@
 #!/usr/bin/env ruby
 require_relative '../src/main/elfname'
 
-ElfName.rand_name
+die = rand(8)
+
+if die < 4
+	ElfName.first_type_one
+elsif die < 6
+	ElfName.first_type_two
+else
+	ElfName.first_type_three
+end
--- src/main/elfname.rb
+++ src/main/elfname.rb
@@ -1,7 +1,7 @@
 require_relative 'loader'
 
 module ElfName
-	def ElfName.rand_name
+	def ElfName.first_type_one
 		first_pref = Loader.load_first_pref
 		first_suff = Loader.load_first_suff
 		pref = first_pref[rand(first_pref.length)]
@@ -18,5 +18,55 @@ module ElfName
 		puts names.join(", ")
 		puts "#{pref.prefixes.join(", ")} means #{pref.meaning}"
 		puts "-#{suff.suffixes.join(", -")} means #{suff.meaning}"
+	end
+	
+	def ElfName.first_type_two
+		first_pref = Loader.load_first_pref
+		first_suff = Loader.load_first_suff
+		pref = first_pref[rand(first_pref.length)]
+		suff_one = first_suff[rand(first_suff.length)]
+		suff_two = first_suff[rand(first_suff.length)]
+
+		names = []
+
+		pref.prefixes.each do |pref_str|
+			suff_one.suffixes.each do |suff_one_str|
+				suff_two.suffixes.each do |suff_two_str|
+					names += [pref_str + suff_one_str + suff_two_str]
+				end
+			end
+		end
+		
+		puts names.join(", ")
+		puts "#{pref.prefixes.join(", ")} means #{pref.meaning}"
+		puts "-#{suff_one.suffixes.join(", -")} means #{suff_one.meaning}"
+		puts "-#{suff_two.suffixes.join(", -")} means #{suff_two.meaning}"
+	end
+	
+	def ElfName.first_type_three
+		first_pref = Loader.load_first_pref
+		first_suff = Loader.load_first_suff
+		suff_one = first_suff[rand(first_suff.length)]
+		pref = first_pref[rand(first_pref.length)]
+		suff_two = first_suff[rand(first_suff.length)]
+		suff_three = first_suff[rand(first_suff.length)]
+
+		names = []
+
+		suff_one.suffixes.each do |suff_one_str|
+			pref.prefixes.each do |pref_str|
+				suff_two.suffixes.each do |suff_two_str|
+					suff_three.suffixes.each do |suff_three_str|
+						names += [suff_one_str + "'" + pref_str + suff_two_str + suff_three_str]
+					end
+				end
+			end
+		end
+
+		puts names.join(", ")
+		puts "-#{suff_one.suffixes.join(", -")} means #{suff_one.meaning}"
+		puts "#{pref.prefixes.join(", ")} means #{pref.meaning}"
+		puts "-#{suff_two.suffixes.join(", -")} means #{suff_two.meaning}"
+		puts "-#{suff_three.suffixes.join(", -")} means #{suff_three.meaning}"
 	end
 end
